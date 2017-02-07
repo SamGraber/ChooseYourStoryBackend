8bb3fb16bc14f2a2f1d8a26ef0a89d54c34f98ea
SamGraber <authorsamgraber@gmail.com>
2017-02-07 14:52:12 -0500

    Added an endpoint for getting the current user's profile. If none exists, take the info passed from auth0 to create a new user profile.

--- api/user/model.ts
+++ api/user/model.ts
@@ -1,13 +1,17 @@
 import { model, Schema, Model, Document } from 'mongoose';
 
 export interface IUser extends Document {
+	oauthId: string;
 	name: string;
+	picture: string;
 	friends: number[];
 	stories: number[];
 }
 
 export const UserSchema = new Schema({
+	oauthId: String,
 	name: String,
+	picture: String,
 	friends: Array,
 	stories: Array,
 });
--- api/user/userRoutes.ts
+++ api/user/userRoutes.ts
@@ -3,6 +3,17 @@ import * as Router from 'koa-router';
 import { IUser, User } from './model';
 
 export function configureUserRoutes(router: Router) {
+	router.get('/api/profile', async (context, next) => {
+		const profileInfo = context.query;
+		await User.findOne({ 'oauthId': profileInfo.oauthId }).then(data => {
+			if (data) {
+				context.body = data;
+			} else {
+				return User.create(profileInfo).then(result => context.body = result);
+			}
+		});
+		next();
+	});
 	router.get('/api/users', async (context, next) => {
 		context.body = await searchUsers();
 		next();
